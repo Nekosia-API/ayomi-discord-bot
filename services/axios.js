f2982b401dfd916a081822d87ed4d3ee6bba053e
Sefinek <contact@sefinek.net>
2024-09-10 23:41:02 +0200

    AyomiBot

--- services/axios.js
+++ services/axios.js
@@ -2,6 +2,6 @@ const axios = require('axios');
 const { version } = require('../package.json');
 
 axios.defaults.timeout = 7000;
-axios.defaults.headers.common['User-Agent'] = `Mozilla/5.0 (compatible; AyomiDiscordBOT/${version}; +https://nekosia.cat)`;
+axios.defaults.headers.common['User-Agent'] = `Mozilla/5.0 (compatible; AyomiBot/${version}; +https://nekosia.cat)`;
 
 module.exports = axios;
