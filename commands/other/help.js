6ae139a724712b9f8c8e1b5b14bad13620e7ee5c
Sefinek <contact@sefinek.net>
2024-09-10 20:29:26 +0200

    Update /help

--- commands/other/help.js
+++ commands/other/help.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Shows all available bot commands with details.'),
 	execute: async (client, inter) => {
 		const embed1 = new EmbedBuilder()
-			.setAuthor({ name: `List of available commands for the ${client.user.username} bot`, iconURL: client.user.displayAvatarURL() })
+			.setAuthor({ name: `List of available commands for ${client.user.username} bot`, iconURL: client.user.displayAvatarURL() })
 			.setColor('#00FFAA')
 			.setDescription('Please see https://nekosia.cat/ayomi/commands.');
 
@@ -16,9 +16,9 @@ module.exports = {
 				{ name: '🌍  Official Website', value: '[nekosia.cat](https://nekosia.cat)', inline: true },
 				{ name: '📝  Documentation', value: '[Documentation](https://nekosia.cat/documentation?page=introduction)', inline: true },
 				{ name: '😺  Status Page', value: '[status.nekosia.cat](https://status.nekosia.cat)', inline: true },
-				{ name: '💗  Add This Bot', value: 'Want to add this bot to your server? Click [here](https://nekosia.cat/ayomi).' },
-				{ name: '🔢  Are You a Developer?', value: 'If so, check out the [Nekosia API documentation](https://nekosia.cat/documentation?page=introduction). This API provides a variety of random anime images. Add some anime magic to your projects today!' },
-				{ name: '😻  Cutest Anime Booru', value: 'Visit our [Anime Booru](https://nekosia.cat/booru) for adorable images! Join our community to rate, comment, and browse through images. You can also submit requests to update image information. Once reviewed, your changes will be reflected in API responses. We greatly appreciate your contributions, as our API relies on accurate image tagging.' }
+				{ name: '💗  Add This Bot', value: 'Want to add this bot to your server? Click [here](https://discord.com/oauth2/authorize?client_id=1282807473769680938)!' },
+				{ name: '🔢  Are You a Developer?', value: 'If so, refer to the [Nekosia API documentation](https://nekosia.cat/documentation?page=introduction). This API provides a variety of random anime images. Add a touch of anime charm to your projects today!' },
+				{ name: '😻  Cutest Anime Booru', value: 'Visit our [anime booru](https://nekosia.cat/booru) for even more cute pictures! Become a member of our community to explore, comment on, and rate images. You can also submit requests to update image data. Once reviewed, your edits will be reflected in the API responses. We greatly appreciate your contributions, as our API relies on accurate image tagging.' }
 			])
 			.setFooter({ text: 'Copyright © 2024 by Nekosia API. All Rights Reserved.', iconURL: client.user.displayAvatarURL() });
 
