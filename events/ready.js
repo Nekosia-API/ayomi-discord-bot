3123dc2d47c82e90931902adbc2e14667d975a98
Sefinek <contact@sefinek.net>
2024-09-10 23:40:20 +0200

    process.send('ready')

--- events/ready.js
+++ events/ready.js
@@ -6,5 +6,12 @@ module.exports = {
 	execute(client) {
 		setActivity(client.user);
 		console.log(`Client » ${client.user.username} is ready!`);
+
+		if (process.env.NODE_ENV === 'development') return;
+		try {
+			process.send('ready');
+		} catch (err) {
+			console.warn('Failed to send ready signal to PM2.', err.message);
+		}
 	}
 };
